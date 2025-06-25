095a510a1fdf6aa508574b1e75b708246e6aa1d9
niiodaie <102459031+niiodaie@users.noreply.github.com>
2025-06-24 20:21:27 -0500

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,14 @@
-import { defineConfig } from 'vite'
+ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: '.', // project root
+  build: {
+    outDir: 'dist', // default, fine for Vercel
+  },
   server: {
-    host: '0.0.0.0',
-    port: 5173
+    port: 5173,
+    host: '0.0.0.0'
   }
 })
-
